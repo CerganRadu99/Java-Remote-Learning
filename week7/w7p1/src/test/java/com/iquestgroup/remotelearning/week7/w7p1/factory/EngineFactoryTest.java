2612e8a04607cecaddd7d1ccbef4ede3ffe29dd8
Cergan Radu Mihai <cerganradumihai@gmail.com>
2021-03-30 18:42:05 +0300

    week 7 p1 fixed: code is now formatted, unused annotations and methods are removed, all non-constant members are initialized inside initData() method and happyflow test check objects inside list too, not only the size of the list

--- week7/w7p1/src/test/java/com/iquestgroup/remotelearning/week7/w7p1/factory/EngineFactoryTest.java
+++ week7/w7p1/src/test/java/com/iquestgroup/remotelearning/week7/w7p1/factory/EngineFactoryTest.java
@@ -1,6 +1,8 @@
 package com.iquestgroup.remotelearning.week7.w7p1.factory;
 
 import com.iquestgroup.remotelearning.week7.w7p1.domain.Employee;
+import com.iquestgroup.remotelearning.week7.w7p1.domain.Engine;
+import com.iquestgroup.remotelearning.week7.w7p1.domain.EngineArchitecture;
 import com.iquestgroup.remotelearning.week7.w7p1.domain.EngineComponent;
 import com.iquestgroup.remotelearning.week7.w7p1.exception.InsufficientStockException;
 import com.iquestgroup.remotelearning.week7.w7p1.exception.UnauthorizedEmployeeException;
@@ -8,48 +10,46 @@ import com.iquestgroup.remotelearning.week7.w7p1.exception.UnqualifiedEmployeeEx
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class EngineFactoryTest {
 
-  public static final Employee UNAUTHORIZED_EMPLOYEE = new Employee("Ionel", true, true);
-  public static final Employee UNQUALIFIED_EMPLOYEE = new Employee("Mircea", true, false);
-  public static final Employee VALID_EMPLOYEE = new Employee("Aurel", true, true);
-  public static final int VALID_NUMBER_OF_ENGINES = 1;
-  public static final int INVALID_NUMBER_OF_ENGINES = 2;
-  public static final int VALID_NO_OF_COMPONENTS_PER_ENGINE = 3;
-  public static final List<Employee> EMPLOYEES = new ArrayList<>(
-      Arrays.asList(
-          UNQUALIFIED_EMPLOYEE, VALID_EMPLOYEE));
-  public static final List<EngineComponent> ENGINE_COMPONENTS = new ArrayList<>(Arrays
-      .asList(new EngineComponent("eng1", 22.2), new EngineComponent("eng2", 31.5),
-          new EngineComponent("eng3", 40.00)));
-  public static final List<EngineComponent> EMPTY_LIST = new ArrayList<>();
+  public static final Employee UNAUTHORIZED_EMPLOYEE;
+  public static final Employee UNQUALIFIED_EMPLOYEE;
+  public static final Employee VALID_EMPLOYEE;
+  public static final Engine ENGINE;
+  public static final int VALID_NUMBER_OF_ENGINES;
+  public static final int INVALID_NUMBER_OF_ENGINES;
+  public static final int VALID_NO_OF_COMPONENTS_PER_ENGINE;
+  public static final List<EngineComponent> EMPTY_LIST;
 
+  static {
+    UNAUTHORIZED_EMPLOYEE = new Employee("Ionel", true, true);
+    UNQUALIFIED_EMPLOYEE = new Employee("Mircea", true, false);
+    VALID_EMPLOYEE = new Employee("Aurel", true, true);
+    ENGINE = new Engine(EngineArchitecture.L4, 2.0, 210);
+    VALID_NUMBER_OF_ENGINES = 1;
+    INVALID_NUMBER_OF_ENGINES = 2;
+    VALID_NO_OF_COMPONENTS_PER_ENGINE = 3;
+    EMPTY_LIST = new ArrayList<>();
+  }
+
+  private List<Employee> employees;
   private EngineFactory engineFactory;
 
   @Before
   public void setup() {
-    engineFactory = new EngineFactory(EMPLOYEES, ENGINE_COMPONENTS);
-  }
-
-  @After
-  public void teardown() {
-    //cleanup database
-    //delete temporary files
-    //etc.
+    initData();
   }
 
   @Test
   public void testGetComponentsPerEngine() {
-    assertEquals("There are no " + VALID_NO_OF_COMPONENTS_PER_ENGINE + " components per engine",
-        VALID_NO_OF_COMPONENTS_PER_ENGINE, EngineFactory.getComponentsPerEngine());
+    int actualResult = EngineFactory.getComponentsPerEngine();
+    String assertMessage = "There are no " + VALID_NO_OF_COMPONENTS_PER_ENGINE + " components per engine.";
+    assertEquals(assertMessage, VALID_NO_OF_COMPONENTS_PER_ENGINE, actualResult);
   }
 
   @Test(expected = UnauthorizedEmployeeException.class)
@@ -69,30 +69,54 @@ public class EngineFactoryTest {
 
   @Test(expected = InsufficientStockException.class)
   public void testManufactureEnginesNullEngineComponents() {
-    engineFactory = new EngineFactory(EMPLOYEES, null);
+    engineFactory = new EngineFactory(employees, null);
     engineFactory.manufactureEngines(VALID_NUMBER_OF_ENGINES, VALID_EMPLOYEE);
   }
 
   @Test(expected = InsufficientStockException.class)
   public void testManufactureEnginesEmptyEngineComponents() {
-    engineFactory = new EngineFactory(EMPLOYEES, EMPTY_LIST);
+    engineFactory = new EngineFactory(employees, EMPTY_LIST);
     engineFactory.manufactureEngines(VALID_NUMBER_OF_ENGINES, VALID_EMPLOYEE);
   }
 
   @Test
   public void testManufactureEngines() {
-    assertEquals("The size is not " + VALID_NUMBER_OF_ENGINES,
-        VALID_NUMBER_OF_ENGINES,
-        engineFactory.manufactureEngines(VALID_NUMBER_OF_ENGINES, VALID_EMPLOYEE).size());
+    List<Engine> expectedEngines = getExpectedEngines();
+    List<Engine> actualEngines = engineFactory.manufactureEngines(VALID_NUMBER_OF_ENGINES, VALID_EMPLOYEE);
+    int expectedSize = expectedEngines.size();
+    int actualSize = actualEngines.size();
+    String assertMessageDifferentSizes = "The size is not " + VALID_NUMBER_OF_ENGINES + ".";
+    assertEquals(assertMessageDifferentSizes, expectedSize, actualSize);
+    for (int index = 0; index < actualSize; index++) {
+      EngineArchitecture expectedEngineArchitecture = expectedEngines.get(index).getEngineArchitecture();
+      EngineArchitecture actualEngineArchitecture = actualEngines.get(index).getEngineArchitecture();
+      double expectedDisplacement = expectedEngines.get(index).getDisplacement();
+      double actualDisplacement = actualEngines.get(index).getDisplacement();
+      int expectedHorsePower = expectedEngines.get(index).getHorsePower();
+      int actualHorsePower = actualEngines.get(index).getHorsePower();
+      assertEquals(expectedEngineArchitecture, actualEngineArchitecture);
+      assertEquals(expectedDisplacement, actualDisplacement, 0);
+      assertEquals(expectedHorsePower, actualHorsePower);
+    }
   }
 
-  @BeforeClass
-  public static void oneTimeSetup() {
-
+  private List<Engine> getExpectedEngines() {
+    List<Engine> expectedEngines = new ArrayList<>();
+    for (int i = 0; i < VALID_NUMBER_OF_ENGINES; i++) {
+      expectedEngines.add(ENGINE);
+    }
+    return expectedEngines;
   }
 
-  @AfterClass
-  public static void oneTimeTearDown() {
-
+  private void initData() {
+    employees = new ArrayList<>(Arrays.asList(UNQUALIFIED_EMPLOYEE, VALID_EMPLOYEE));
+    EngineComponent engineComponent1 = new EngineComponent("eng1", 22.2);
+    EngineComponent engineComponent2 = new EngineComponent("eng2", 28.9);
+    EngineComponent engineComponent3 = new EngineComponent("eng3", 34.3);
+    List<EngineComponent> engineComponents = new ArrayList<>();
+    engineComponents.add(engineComponent1);
+    engineComponents.add(engineComponent2);
+    engineComponents.add(engineComponent3);
+    engineFactory = new EngineFactory(employees, engineComponents);
   }
 }
