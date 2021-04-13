08cb26007f31b9b2619fcc5640ef6a8013cf9770
Cergan Radu Mihai <cerganradumihai@gmail.com>
2021-04-13 23:39:55 +0300

    p2 updated: works with any implementation of Collection interface, sort methods return sorted collection(not void and variables renamed

--- week9/w9p2/src/main/java/com/iquestgroup/remotelearning/week9/w9p2/GenericBubbleSort.java
+++ week9/w9p2/src/main/java/com/iquestgroup/remotelearning/week9/w9p2/GenericBubbleSort.java
@@ -1,20 +1,43 @@
 package com.iquestgroup.remotelearning.week9.w9p2;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class GenericBubbleSort {
 
-  public <T extends Comparable<T>> void sort(T[] collection) {
-    for (int i = 0; i < collection.length - 1; i++) {
-      for (int j = 0; j < collection.length - i - 1; j++) {
-        if (collection[j].compareTo(collection[j + 1]) > 0) {
-          swap(collection, j, j + 1);
+  public <T extends Comparable<T>> T[] sort(T[] array) {
+    for (int i = 0; i < array.length - 1; i++) {
+      for (int j = 0; j < array.length - i - 1; j++) {
+        if (array[j].compareTo(array[j + 1]) > 0) {
+          changePositionFromArray(array, j);
         }
       }
     }
+    return array;
   }
 
-  private <T> void swap(T[] collection, int i, int j) {
-    T temp = collection[i];
-    collection[i] = collection[j];
-    collection[j] = temp;
+  public <T extends Comparable<T>> Collection<T> sort(Collection<T> collection) {
+    List<T> listToSort = new ArrayList<>(collection);
+    for (int i = 0; i < listToSort.size() - 1; i++) {
+      for (int j = 0; j < listToSort.size() - i - 1; j++) {
+        if (listToSort.get(j).compareTo(listToSort.get(j + 1)) > 0) {
+          changePositionFromCollection(listToSort, j);
+        }
+      }
+    }
+    return listToSort;
+  }
+
+  private <T> void changePositionFromArray(T[] array, int positionToChange) {
+    T objectToMove = array[positionToChange];
+    array[positionToChange] = array[positionToChange + 1];
+    array[positionToChange + 1] = objectToMove;
+  }
+
+  private <T> void changePositionFromCollection(List<T> collection, int positionToChange) {
+    T objectToMove = collection.get(positionToChange);
+    collection.set(positionToChange, collection.get(positionToChange + 1));
+    collection.set(positionToChange + 1, objectToMove);
   }
 }
--- week9/w9p2/src/test/java/com/iquestgroup/remotelearning/week9/w9p2/GenericBubbleSortTest.java
+++ week9/w9p2/src/test/java/com/iquestgroup/remotelearning/week9/w9p2/GenericBubbleSortTest.java
@@ -2,27 +2,42 @@ package com.iquestgroup.remotelearning.week9.w9p2;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
 public class GenericBubbleSortTest {
 
-  public static final Integer[] SORTED_INTEGER_COLLECTION;
-  public static final Double[] SORTED_DOUBLE_COLLECTION;
-  public static final String[] SORTED_STRING_COLLECTION;
-  public static final Person[] SORTED_PERSON_COLLECTION;
+  public static final List<Integer> SORTED_INTEGER_COLLECTION;
+  public static final List<Double> SORTED_DOUBLE_COLLECTION;
+  public static final List<String> SORTED_STRING_COLLECTION;
+  public static final List<Person> SORTED_PERSON_COLLECTION;
+  public static final Integer[] SORTED_INTEGER_ARRAY;
+  public static final Double[] SORTED_DOUBLE_ARRAY;
+  public static final String[] SORTED_STRING_ARRAY;
+  public static final Person[] SORTED_PERSON_ARRAY;
 
   static {
-    SORTED_INTEGER_COLLECTION = new Integer[]{-103, -12, -9, 0, 9, 13, 20, 31, 45, 99, 99, 234};
-    SORTED_DOUBLE_COLLECTION = new Double[]{-45.83, -34.12, -3.45, -2.2, 3.13, 4.9, 8.01, 12.11, 13.99, 99.78};
-    SORTED_STRING_COLLECTION = new String[]{"actually", "calculating", "defective", "disgusting", "plantation", "plucky", "snake", "surround",
-        "tacit", "three"};
-    SORTED_PERSON_COLLECTION = new Person[]{new Person("Darius", 8), new Person("Ionel", 19), new Person("Radu", 21), new Person("Gigel", 31),
+    SORTED_INTEGER_ARRAY = new Integer[]{-103, -12, -9, 0, 9, 13, 20, 31, 45, 99, 99, 234};
+    SORTED_DOUBLE_ARRAY = new Double[]{-45.83, -34.12, -3.45, -2.2, 3.13, 4.9, 8.01, 12.11, 13.99, 99.78};
+    SORTED_STRING_ARRAY = new String[]{"actually", "calculating", "defective", "disgusting", "plantation", "plucky", "snake", "surround", "tacit",
+        "three"};
+    SORTED_PERSON_ARRAY = new Person[]{new Person("Darius", 8), new Person("Ionel", 19), new Person("Radu", 21), new Person("Gigel", 31),
         new Person("Marian", 45)};
+    SORTED_INTEGER_COLLECTION = new ArrayList<>(Arrays.asList(SORTED_INTEGER_ARRAY));
+    SORTED_DOUBLE_COLLECTION = new LinkedList<>(Arrays.asList(SORTED_DOUBLE_ARRAY));
+    SORTED_STRING_COLLECTION = new ArrayList<>(Arrays.asList(SORTED_STRING_ARRAY));
+    SORTED_PERSON_COLLECTION = new LinkedList<>(Arrays.asList(SORTED_PERSON_ARRAY));
   }
 
   private GenericBubbleSort genericBubbleSort;
-  
+
   @Before
   public void setup() {
     initData();
@@ -30,48 +45,97 @@ public class GenericBubbleSortTest {
 
   @Test
   public void testBubbleSortCollectionOfIntegers() {
-    Integer[] integerCollection = new Integer[]{9, -12, 13, 45, 99, -103, 234, 31, 99, 20, -9, 0};
-    genericBubbleSort.sort(integerCollection);
-    for (int index = 0; index < integerCollection.length; index++) {
-      int expectedResult = SORTED_INTEGER_COLLECTION[index];
-      int actualResult = integerCollection[index];
+    Collection<Integer> unsortedIntegerCollection = new ArrayList<>(Arrays.asList(9, -12, 13, 45, 99, -103, 234, 31, 99, 20, -9, 0));
+    List<Integer> sortedIntegerCollection = (List<Integer>) genericBubbleSort.sort(unsortedIntegerCollection);
+    for (int index = 0; index < sortedIntegerCollection.size(); index++) {
+      int expectedResult = SORTED_INTEGER_COLLECTION.get(index);
+      int actualResult = sortedIntegerCollection.get(index);
       assertEquals(expectedResult, actualResult);
     }
   }
 
   @Test
   public void testBubbleSortCollectionOfDoubles() {
-    Double[] doubleCollection = new Double[]{-2.2, 3.13, 4.90, 12.11, -3.45, 8.01, 99.78, -34.12, 13.99, -45.83};
-    genericBubbleSort.sort(doubleCollection);
-    for (int index = 0; index < doubleCollection.length; index++) {
-      double expectedResult = SORTED_DOUBLE_COLLECTION[index];
-      double actualResult = doubleCollection[index];
+    Collection<Double> unsortedDoubleCollection = new LinkedList<>(Arrays.asList(-2.2, 3.13, 4.90, 12.11, -3.45, 8.01, 99.78, -34.12, 13.99, -45.83));
+    List<Double> sortedDoubleCollection = (List<Double>) genericBubbleSort.sort(unsortedDoubleCollection);
+    for (int index = 0; index < sortedDoubleCollection.size(); index++) {
+      double expectedResult = SORTED_DOUBLE_COLLECTION.get(index);
+      double actualResult = sortedDoubleCollection.get(index);
       assertEquals(expectedResult, actualResult, 0);
     }
   }
 
   @Test
   public void testBubbleSortCollectionOfStrings() {
-    String[] stringCollection = new String[]{"surround", "three", "plucky", "defective", "snake", "tacit", "disgusting", "actually", "calculating",
-        "plantation"};
-    genericBubbleSort.sort(stringCollection);
-    for (int index = 0; index < stringCollection.length; index++) {
-      String expectedResult = SORTED_STRING_COLLECTION[index];
-      String actualResult = stringCollection[index];
+    Collection<String> unsortedStringCollection = new HashSet<>(
+        Arrays.asList("surround", "three", "plucky", "defective", "snake", "tacit", "disgusting", "actually", "calculating", "plantation"));
+    List<String> sortedStringCollection = (List<String>) genericBubbleSort.sort(unsortedStringCollection);
+    for (int index = 0; index < sortedStringCollection.size(); index++) {
+      String expectedResult = SORTED_STRING_COLLECTION.get(index);
+      String actualResult = sortedStringCollection.get(index);
       assertEquals(expectedResult, actualResult);
     }
   }
 
   @Test
   public void testBubbleSortCollectionOfPersons() {
-    Person[] personCollection = new Person[]{new Person("Marian", 45), new Person("Gigel", 31), new Person("Radu", 21), new Person("Darius", 8),
+    Collection<Person> unsortedPersonCollection = new LinkedHashSet<>(
+        Arrays.asList(new Person("Marian", 45), new Person("Gigel", 31), new Person("Radu", 21), new Person("Darius", 8), new Person("Ionel", 19)));
+    List<Person> sortedPersonCollection = (List<Person>) genericBubbleSort.sort(unsortedPersonCollection);
+    for (int index = 0; index < sortedPersonCollection.size(); index++) {
+      String expectedName = SORTED_PERSON_COLLECTION.get(index).getName();
+      String actualName = sortedPersonCollection.get(index).getName();
+      int expectedAge = SORTED_PERSON_COLLECTION.get(index).getAge();
+      int actualAge = sortedPersonCollection.get(index).getAge();
+      assertEquals(expectedName, actualName);
+      assertEquals(expectedAge, actualAge);
+    }
+  }
+
+  @Test
+  public void testBubbleSortArrayOfIntegers() {
+    Integer[] unsortedIntegerArray = new Integer[]{9, -12, 13, 45, 99, -103, 234, 31, 99, 20, -9, 0};
+    Integer[] sortedIntegerArray = genericBubbleSort.sort(unsortedIntegerArray);
+    for (int index = 0; index < sortedIntegerArray.length; index++) {
+      int expectedResult = SORTED_INTEGER_ARRAY[index];
+      int actualResult = sortedIntegerArray[index];
+      assertEquals(expectedResult, actualResult);
+    }
+  }
+
+  @Test
+  public void testBubbleSortArrayOfDoubles() {
+    Double[] unsortedDoubleArray = new Double[]{-2.2, 3.13, 4.90, 12.11, -3.45, 8.01, 99.78, -34.12, 13.99, -45.83};
+    Double[] sortedDoubleArray = genericBubbleSort.sort(unsortedDoubleArray);
+    for (int index = 0; index < sortedDoubleArray.length; index++) {
+      double expectedResult = SORTED_DOUBLE_ARRAY[index];
+      double actualResult = sortedDoubleArray[index];
+      assertEquals(expectedResult, actualResult, 0);
+    }
+  }
+
+  @Test
+  public void testBubbleSortArrayOfStrings() {
+    String[] unsortedStringArray = new String[]{"surround", "three", "plucky", "defective", "snake", "tacit", "disgusting", "actually", "calculating",
+        "plantation"};
+    String[] sortedStringArray = genericBubbleSort.sort(unsortedStringArray);
+    for (int index = 0; index < sortedStringArray.length; index++) {
+      String expectedResult = SORTED_STRING_ARRAY[index];
+      String actualResult = sortedStringArray[index];
+      assertEquals(expectedResult, actualResult);
+    }
+  }
+
+  @Test
+  public void testBubbleSortArrayOfPersons() {
+    Person[] unsortedPersonArray = new Person[]{new Person("Marian", 45), new Person("Gigel", 31), new Person("Radu", 21), new Person("Darius", 8),
         new Person("Ionel", 19)};
-    genericBubbleSort.sort(personCollection);
-    for (int index = 0; index < personCollection.length; index++) {
-      String expectedName = SORTED_PERSON_COLLECTION[index].getName();
-      String actualName = personCollection[index].getName();
-      int expectedAge = SORTED_PERSON_COLLECTION[index].getAge();
-      int actualAge = personCollection[index].getAge();
+    Person[] sortedPersonArray = genericBubbleSort.sort(unsortedPersonArray);
+    for (int index = 0; index < sortedPersonArray.length; index++) {
+      String expectedName = SORTED_PERSON_ARRAY[index].getName();
+      String actualName = sortedPersonArray[index].getName();
+      int expectedAge = SORTED_PERSON_ARRAY[index].getAge();
+      int actualAge = sortedPersonArray[index].getAge();
       assertEquals(expectedName, actualName);
       assertEquals(expectedAge, actualAge);
     }
