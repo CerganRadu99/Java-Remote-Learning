64f8fc0a84260a023ac5ac053b4c243face3d1b4
Cergan Radu Mihai <cerganradumihai@gmail.com>
2021-04-14 12:58:11 +0300

    p3 updated: variables and methods renamed, unhappy flow tested, Sorter updated(return a different list, not the same list received as parameter)

---
+++ week9/w9p3/src/main/java/com/iquestgroup/remotelearning/week9/w9p3/exceptions/GenericPriorityQueueEmptyException.java
@@ -0,0 +1,8 @@
+package com.iquestgroup.remotelearning.week9.w9p3.exceptions;
+
+public class GenericPriorityQueueEmptyException extends RuntimeException {
+
+  public GenericPriorityQueueEmptyException(String message) {
+    super(message);
+  }
+}
---
+++ week9/w9p3/src/main/java/com/iquestgroup/remotelearning/week9/w9p3/exceptions/GenericPriorityQueueFullException.java
@@ -0,0 +1,8 @@
+package com.iquestgroup.remotelearning.week9.w9p3.exceptions;
+
+public class GenericPriorityQueueFullException extends RuntimeException {
+
+  public GenericPriorityQueueFullException(String message) {
+    super(message);
+  }
+}
