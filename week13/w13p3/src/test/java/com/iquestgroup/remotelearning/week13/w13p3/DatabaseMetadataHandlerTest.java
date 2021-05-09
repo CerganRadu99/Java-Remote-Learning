7c43be081a2aadfb472c2f8b3ebf9e96d1b74877
Cergan Radu Mihai <cerganradumihai@gmail.com>
2021-05-09 19:00:45 +0300

    p3 updated: read data for accessing database from file

--- week13/w13p3/src/test/java/com/iquestgroup/remotelearning/week13/w13p3/DatabaseMetadataHandlerTest.java
+++ week13/w13p3/src/test/java/com/iquestgroup/remotelearning/week13/w13p3/DatabaseMetadataHandlerTest.java
@@ -2,8 +2,11 @@ package com.iquestgroup.remotelearning.week13.w13p3;
 
 import static org.junit.Assert.assertEquals;
 
+import com.iquestgroup.remotelearning.week13.w13p3.exception.DatabaseAccessDataFileNotFoundException;
 import com.iquestgroup.remotelearning.week13.w13p3.exception.DatabaseException;
 import com.iquestgroup.remotelearning.week13.w13p3.models.TableColumn;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,29 +15,38 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class DatabaseMetadataHandlerTest {
 
+  public static final int DRIVER_PATH_INDEX = 0;
+  public static final int URL_INDEX = 1;
+  public static final int USER_INDEX = 2;
+  public static final int PASSWORD_INDEX = 3;
+  public static final String FILE_PATH = "src/test/resources/DatabaseAccessData.txt";
+  public static final String STRING_DELIMITER = "-";
+  public static final String DATABASE_ACCESS_DATA_EXCEPTION_MESSAGE = "File not found!";
   public static final List<String> DATABASE_TABLES = new ArrayList<>(Arrays.asList("client", "transaction"));
   public static final Map<String, List<TableColumn>> TABLES;
   public static final int NUMBER_OF_TABLES = 2;
   public static final String DATABASE_NAME = "mydb";
-  public static final String DRIVER_PATH = "com.mysql.cj.jdbc.Driver";
-  public static final String URL = "jdbc:mysql://localhost:3306/mydb";
-  public static final String USER = "test";
-  public static final String PASSWORD = "test";
+  public static String driverPath;
+  public static String url;
+  public static String user;
+  public static String password;
   private static Connection databaseConnection;
 
   static {
     List<TableColumn> clientColumns = new ArrayList<>();
     clientColumns.add(new TableColumn("id", "INT", 10));
-    clientColumns.add(new TableColumn("username", "VARCHAR", 45));
+    clientColumns.add(new TableColumn("username", "VARCHAR", 20));
     clientColumns.add(new TableColumn("balance", "DECIMAL", 13));
     List<TableColumn> transactionColumns = new ArrayList<>();
     transactionColumns.add(new TableColumn("id", "INT", 10));
-    transactionColumns.add(new TableColumn("name", "VARCHAR", 45));
+    transactionColumns.add(new TableColumn("name", "VARCHAR", 20));
     transactionColumns.add(new TableColumn("amount", "DECIMAL", 13));
     transactionColumns.add(new TableColumn("client_id", "INT", 10));
     TABLES = new HashMap<>();
@@ -43,6 +55,33 @@ public class DatabaseMetadataHandlerTest {
   }
 
   private DatabaseMetadataHandler databaseMetadataHandler;
+
+  @BeforeClass
+  public static void setupBeforeClass() {
+    readDatabaseAccessData();
+  }
+
+  private static void readDatabaseAccessData() {
+    List<String> databaseAccessData = new ArrayList<>();
+    File databaseAccessDataFile = new File(FILE_PATH);
+    try {
+      Scanner fileReader = new Scanner(databaseAccessDataFile);
+      while (fileReader.hasNextLine()) {
+        String currentData = fileReader.nextLine();
+        databaseAccessData.add(currentData.substring(currentData.lastIndexOf(STRING_DELIMITER) + 1));
+      }
+      setDatabaseAccesData(databaseAccessData);
+    } catch (FileNotFoundException exception) {
+      throw new DatabaseAccessDataFileNotFoundException(DATABASE_ACCESS_DATA_EXCEPTION_MESSAGE);
+    }
+  }
+
+  private static void setDatabaseAccesData(List<String> databaseAccessData) {
+    driverPath = databaseAccessData.get(DRIVER_PATH_INDEX);
+    url = databaseAccessData.get(URL_INDEX);
+    user = databaseAccessData.get(USER_INDEX);
+    password = databaseAccessData.get(PASSWORD_INDEX);
+  }
 
   @Before
   public void setup() {
@@ -84,8 +123,8 @@ public class DatabaseMetadataHandlerTest {
 
   private void initData() {
     try {
-      Class.forName(DRIVER_PATH);
-      databaseConnection = DriverManager.getConnection(URL, USER, PASSWORD);
+      Class.forName(driverPath);
+      databaseConnection = DriverManager.getConnection(url, user, password);
     } catch (ClassNotFoundException | SQLException exception) {
       exception.printStackTrace();
     }
---
+++ week13/w13p3/src/test/java/com/iquestgroup/remotelearning/week13/w13p3/exception/DatabaseAccessDataFileNotFoundException.java
@@ -0,0 +1,8 @@
+package com.iquestgroup.remotelearning.week13.w13p3.exception;
+
+public class DatabaseAccessDataFileNotFoundException extends RuntimeException {
+
+  public DatabaseAccessDataFileNotFoundException(String message) {
+    super(message);
+  }
+}
